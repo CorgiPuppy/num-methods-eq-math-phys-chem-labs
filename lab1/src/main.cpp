77423d69a667b5a2a6833bd7b6e3591fae09ff3f
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-02 22:54:46 +0300

    fix: from delta t = 0.1 to 0.001.

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -9,42 +9,23 @@ int main() {
 	for (int i = 0; i < Constants::amount_of_delta_t; i++)
 		N_t[i] = (Constants::t_end - Constants::t_start) / Constants::delta_t[i];
 
-	double u[N_x][N_t[0]] = {0.0};
+	double u[N_t[0]][N_x] = {0.0};
 	for (int j = 0; j <= N_x - 1; j++) {
 		u[0][j] = std::exp((j - 1) * Constants::h);
 	}	
 
 	int n = 0;
-	for (int j = 1; j <= N_x - 2; j++)
-				u[n + 1][j] = u[n][j] + (Constants::delta_t[0]) / (std::pow(Constants::h, 2)) * (u[n][j + 1] - 2 * u[n][j] + u[n][j - 1]);
-			
-			u[n + 1][0] = std::exp((n + 1) * Constants::delta_t[0]);
-			u[n + 1][N_x - 1] = std::exp((n + 1) * Constants::delta_t[0] + 1);
-	n++;
-	for (int j = 1; j <= N_x - 2; j++)
-		u[n + 1][j] = u[n][j] + (Constants::delta_t[0]) / (std::pow(Constants::h, 2)) * (u[n][j + 1] - 2 * u[n][j] + u[n][j - 1]);
-	
-	u[n + 1][0] = std::exp((n + 1) * Constants::delta_t[0]);
-	u[n + 1][N_x - 1] = std::exp((n + 1) * Constants::delta_t[0] + 1);
-	n++;
-	for (int j = 1; j <= N_x - 2; j++)
-		u[n + 1][j] = u[n][j] + (Constants::delta_t[0]) / (std::pow(Constants::h, 2)) * (u[n][j + 1] - 2 * u[n][j] + u[n][j - 1]);
-	
-	u[n + 1][0] = std::exp((n + 1) * Constants::delta_t[0]);
-	u[n + 1][N_x - 1] = std::exp((n + 1) * Constants::delta_t[0] + 1);
 
-
-/*
 	while (!(n == (N_t[0] - 1))) {
 		for (int j = 1; j <= N_x - 2; j++)
-			u[n + 1][j] = u[n][j] + (Constants::delta_t[0]) / (std::pow(Constants::h, 2)) * (u[n][j + 1] - 2 * u[n][j] + u[n][j - 1]);
+			u[n + 1][j] = u[n][j] + (Constants::delta_t[2]) / (std::pow(Constants::h, 2)) * (u[n][j + 1] - 2 * u[n][j] + u[n][j - 1]);
 		
-		u[n + 1][0] = std::exp((n + 1) * Constants::delta_t[0]);
-		u[n + 1][N_x - 1] = std::exp((n + 1) * Constants::delta_t[0] + 1);
+		u[n + 1][0] = std::exp((n + 1) * Constants::delta_t[2]);
+		u[n + 1][N_x - 1] = std::exp((n + 1) * Constants::delta_t[2] + 1);
 	
 		n++;
 	}
-*/
+
 	for (int n = 0; n < N_t[0]; n++) {
 		for (int j = 0; j < N_x; j++)
 		       printf("%.3f ", u[n][j]);
