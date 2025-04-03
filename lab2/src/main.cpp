1ad0fb16bd679c225d59031be561e6b081ea4f52
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-03 10:29:07 +0300

    feat: add a work with write to file.

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 #include "../include/Constants.h"
 
 int main() {
-	int N_x = (Constants::x_end - Constants::x_start) / Constants::h;
+	int N_x = 1 + (Constants::x_end - Constants::x_start) / Constants::h;
 	int N_t[Constants::amount_of_delta_t] = {0};
 	for (int i = 0; i < Constants::amount_of_delta_t; i++)
-		N_t[i] = (Constants::t_end - Constants::t_start) / Constants::delta_t[i];
+		N_t[i] = 1 + (Constants::t_end - Constants::t_start) / Constants::delta_t[i];
 
 	double u[N_x][N_t[0]] = {0.0};
 	for (int j = 0; j <= N_x - 1; j++) {
-		u[0][j] = std::exp((j - 1) * Constants::h);
+		u[0][j] = std::exp(j * Constants::h);
 	}	
 
 	double a[N_x] = {0};
@@ -43,12 +44,17 @@ int main() {
 
 		n++;
 	}
-
+	
+	std::ofstream file (Constants::path);
 	for (int n = 0; n < N_t[0]; n++) {
-		for (int j = 0; j < N_x; j++)
-		       printf("%.3f ", u[n][j]);
-		std::cout << std::endl;
+		double t = (n + 1) * Constants::delta_t[2];
+		for (int j = 0; j < N_x; j++) {
+			double x = j * Constants::h;
+			file << t << " " << x << " " << u[n][j] << "\n";
+		}
+		file << "\n";
 	}
+	file.close();
 
 	return 0;
 }
